742cacb029b1c6f57147b054be369cd206491c21
DoanJs <viethungtrieuphong1994@gmail.com>
2026-06-25 15:55:42 +0700

    init NTEdu P2

--- HoaBanMaiEdu/functions/src/types.ts
+++ HoaBanMaiEdu/functions/src/types.ts
@@ -15,11 +15,3 @@ export interface Report {
   teacherIds: string[];
   status: string
 }
-// https://api.telegram.org/bot   /getUpdates
-// - https://api.telegram.org/bot8824321052:AAHcALH25Jlyr5wJH_7d6CXEoIFel_PdzhE/getWebhookInfo
-// - https://api.telegram.org/bot   /deleteWebhook
-// - https://api.telegram.org/bot8824321052:AAHcALH25Jlyr5wJH_7d6CXEoIFel_PdzhE/setWebhook?url=https://telegramwebhook-wka74oo7xa-as.a.run.app
-// https://api.telegram.org/bot8824321052:AAHcALH25Jlyr5wJH_7d6CXEoIFel_PdzhE/sendMessage?chat_id=8338357435&text=abc 123
-
-// 8824321052:AAHcALH25Jlyr5wJH_7d6CXEoIFel_PdzhE
-// lfjyUePKV5hLwwFCMxWZzn87BF53
